76b681a4237d5c839e0dd8bf0ae857d6113d5d58
elnura <adibaeva2010@gmail.com>
2022-08-29 19:08:14 -0500

    Changes are made

--- src/day2/Login.java
+++ src/day2/Login.java
@@ -3,5 +3,9 @@ package day2;
 public class Login {
     public static void main(String[] args) {
         //page is created
+
+        // I'm working on Login page
+
+        //I am about to complete a bootcamp successfully
     }
 }
